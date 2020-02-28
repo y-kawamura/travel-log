12025ef72c392906becd50ed81e4ec1b4ceb4a89
y-kawamura <tetora.y@gmail.com>
2020-02-28 20:50:28 +0900

    Create to get LogEntry

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -4,10 +4,13 @@ const LogEntry = require('../models/LogEntry');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    message: '🐧',
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    const entries = await LogEntry.find();
+    res.json(entries);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res, next) => {
