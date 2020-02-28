7fce131e9c6c724e09667308b53d4547853f2eb8
y-kawamura <tetora.y@gmail.com>
2020-02-28 16:05:45 +0900

    Refactoring (create a middleware module)

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const middleware = require('./middleware');
+
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
@@ -16,21 +18,8 @@ app.get('/', (req, res) => {
   });
 });
 
-app.use((req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-});
-
-// eslint-disable-next-line
-app.use((error, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? '' : error.stack,
-  });
-});
+app.use(middleware.notFount);
+app.use(middleware.errorHandler);
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
---
+++ server/src/middleware.js
@@ -0,0 +1,20 @@
+const notFount = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+// eslint-disable-next-line
+const errorHandler = (error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '' : error.stack,
+  });
+};
+
+module.exports = {
+  notFount,
+  errorHandler,
+};
