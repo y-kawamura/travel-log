96051409fe148006874d96f2a3d099c1546bd650
y-kawamura <tetora.y@gmail.com>
2020-02-29 11:01:21 +0900

    Change map pin icon

--- client/src/App.js
+++ client/src/App.js
@@ -33,21 +33,8 @@ const App = () => {
             key={entry._id}
             latitude={entry.latitude}
             longitude={entry.longitude}
-            offsetLeft={-12}
-            offsetTop={-24}
           >
-            <svg 
-              viewBox="0 0 24 24"
-              width="24px"
-              height="24px"
-              stroke="#c0392b"
-              stroke-width="1.5"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle>
-            </svg>
+            <img className="marker" src="https://i.imgur.com/y0G5YTX.png" alt={entry.title} />
           </Marker>
         ))
       }
