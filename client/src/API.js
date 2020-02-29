e22ed91ecfce636084de693e7467102741a5d5b3
y-kawamura <tetora.y@gmail.com>
2020-02-29 18:32:22 +0900

    create now config

--- client/src/API.js
+++ client/src/API.js
@@ -1,4 +1,6 @@
-const API_URL = 'http://localhost:1337';
+const API_URL = window.location.hostname === 'localhost'
+  ? 'http://localhost:1337'
+  : 'https://penta-travel-log-api.now.sh';
 
 export async function listLogEntries() {
   const response = await fetch(`${API_URL}/api/logs`);
