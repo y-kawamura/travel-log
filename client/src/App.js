b1e6f79e21051e682936d50a5344fda33dc4e16c
y-kawamura <tetora.y@gmail.com>
2020-02-29 10:48:09 +0900

    Create mapbox in react app. Show the pin of log entries.

---
+++ client/src/App.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+
+import { listLogEntries } from './API';
+
+const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 35.027316,
+    longitude: 135.798238,
+    zoom: 12
+  });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      setLogEntries(logEntries);
+    })();
+  }, []);
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      mapStyle="mapbox://styles/tetora/ck76t3gmt0qdp1jpgv76e893q"
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={setViewport}
+    >
+      {
+        logEntries.map(entry => (
+          <Marker
+            key={entry._id}
+            latitude={entry.latitude}
+            longitude={entry.longitude}
+            offsetLeft={-12}
+            offsetTop={-24}
+          >
+            <svg 
+              viewBox="0 0 24 24"
+              width="24px"
+              height="24px"
+              stroke="#c0392b"
+              stroke-width="1.5"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle>
+            </svg>
+          </Marker>
+        ))
+      }
+    </ReactMapGL>
+  );
+}
+
+export default App;
